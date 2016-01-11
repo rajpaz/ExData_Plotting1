a22651dad31e2e216eb31fbaea55423be004e23b
Rajesh Pazhyannur <rpazhyannur@hotmail.com>
2016-01-10 16:20:30 -0800

    first working version

--- plot1.R
+++ plot1.R
@@ -2,9 +2,9 @@
 source("./getFebData.R")
 
 # Plot -1
+png("./Plot1.png", width = 480, height = 480)
 if (!exists("febData"))
   febData = getFebData()
 hist(febData$Global_active_power,main="Global Active Power for Feb 2007-02-02/03",
      xlab="Global Active Power (kilowatts)",col='red')
-dev.copy(png, file = "Plot1.png")
 dev.off()
