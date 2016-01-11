a307b4ce1a20877ba4732ff51826bc47e962d434
Rajesh Pazhyannur <rpazhyannur@hotmail.com>
2016-01-10 16:22:16 -0800

    changed device to png

--- plot1.R
+++ plot1.R
@@ -5,6 +5,6 @@ source("./getFebData.R")
 png("./Plot1.png", width = 480, height = 480)
 if (!exists("febData"))
   febData = getFebData()
-hist(febData$Global_active_power,main="Global Active Power for Feb 2007-02-02/03",
+hist(febData$Global_active_power,main="Global Active Power",
      xlab="Global Active Power (kilowatts)",col='red')
 dev.off()
